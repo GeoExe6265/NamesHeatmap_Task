e9e79dbb165da22199a9f931b2f4975675efe24d
GeoExe <132460geoexe@users.no-reply.gitverse.ru>
2025-10-19 09:26:09 +0000

    uploaded files
    Uploaded files with Heatmap Task

---
+++ HeatmapTask/HeatmapTask.cs
@@ -0,0 +1,31 @@
+﻿namespace Names;
+
+internal static class HeatmapTask
+{
+    public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+    {
+        var x = new string[30];
+        var y = new string[12];
+        var heat = new double[30, 12];
+        for (var days = 2; days <= 31; days++)
+        {
+            x[days - 2] = days.ToString();
+        }
+
+        for (var months = 1; months <= 12; months++)
+        {
+            y[months - 1] = months.ToString();
+        }
+
+        foreach (var person in names)
+        {
+            if (person.BirthDate.Day != 1)
+            {
+                var day = person.BirthDate.Day - 2;
+                var month = person.BirthDate.Month - 1;
+                heat[day, month] += 1;
+            }
+        }
+        return new HeatmapData("Карта интенсивности рождаемости", heat, x, y);
+    }
+}
